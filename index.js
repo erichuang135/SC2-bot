3aa88d2dc45a320b4666b8ef50354d8c5252e2e8
royalalchemist <erichuang135@gmail.com>
2018-07-24 08:18:22 -0400

    jokes

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on("message", async message =>{
     }
 //begin Joke command
     if (cmd === `${prefix}joke`){
-        var joke = 33//Math.floor((Math.random() * 33) + 1);
+        var joke = Math.floor((Math.random() * 33) + 1);
         if (joke === 1)
             {return message.channel.send("Why did the colossus fall over? *Because it was imbalanced.*");}
         else if (joke === 2)
